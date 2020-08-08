1bce92061bfbfdba06ff2c5efa29d7a4034d97e8
Saiprakash Holi <saiprakash.holi@in.abb.com>
2020-08-08 16:52:36 +0530

    path asset chnaged

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -33,5 +33,5 @@ export class CoreModule { }
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
