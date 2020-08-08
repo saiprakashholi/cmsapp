f02a8675a0d3fcf45d646d5daeee73105d19c730
Saiprakash Holi <saiprakash.holi@in.abb.com>
2020-08-08 15:46:35 +0530

    added login comp

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent, HeaderComponent, FooterComponent],
+  declarations: [HomeComponent, HeaderComponent, FooterComponent, LoginComponent],
   imports: [
     CommonModule
   ]
